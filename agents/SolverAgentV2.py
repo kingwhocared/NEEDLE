b9bafe810daa0e929e34f003f8005a508439efc9
LAPTOP-FETDIP9P\vikto <thevikckick@gmail.com>
2025-02-15 14:08:29 +0200

    Identification of flaws is better, but still too insistent.

--- agents/SolverAgentV2.py
+++ agents/SolverAgentV2.py
@@ -105,7 +105,7 @@ class SolverAgent:
         self.calculator_agent = CalculatorAgent()
 
     @staticmethod
-    def inspect_for_logical_error(context, purpose_of_compute_req, num_1_desc, num_2_desc, operation):
+    def inspect_for_logical_error(context, purpose_of_compute_req, num_1_desc, num_1, num_2_desc, num_2, operation):
         words = {
             "add": "add",
             "subtract": "subtract",
@@ -113,11 +113,11 @@ class SolverAgent:
             "divide": "divide by"
         }
         operation_desc = words.get(operation, "unknown operation")
-        proposal_desc = f"Take {num_1_desc} and {operation_desc} {num_2_desc}"
+        proposal_desc = f"Take {num_1_desc}({num_1}) and {operation_desc} {num_2_desc}({num_2})"
         inspection_res = get_openai_inference_with_schema(
             f"Context: {context}. "
             f"Without attempting any arithmetic computation, "
-            f"Determine if there is a logical error in the following: "
+            f"Determine if the following has a fatal logical error: "
             f"In order to calculate {purpose_of_compute_req}: " +
             proposal_desc
             ,
@@ -157,7 +157,9 @@ class SolverAgent:
                 messages.append(completion.message)
 
                 operation = tool_arguments["operation"]
-                compute_request = (operation, tool_arguments["num1"], tool_arguments["num2"])
+                num1 = tool_arguments["num1"]
+                num2 = tool_arguments["num2"]
+                compute_request = (operation, num1, num2)
                 purpose_or_meaning = tool_arguments["meaning of operation result"]
                 num1_meaning = tool_arguments["num1 meaning"]
                 num2_meaning = tool_arguments["num2 meaning"]
@@ -165,7 +167,7 @@ class SolverAgent:
                 logger.log(
                     f"LLM requested compute request: {compute_request}, 'request_explanation': {request_explanation}")
 
-                logical_error_detected, logical_error_description = self.inspect_for_logical_error(solve_request, purpose_or_meaning, num1_meaning, num2_meaning, operation)
+                logical_error_detected, logical_error_description = self.inspect_for_logical_error(solve_request, purpose_or_meaning, num1_meaning, num1, num2_meaning, num2, operation)
                 if logical_error_detected:
                     # return failure and explain the error.
                     logger.log(f"Detected logical error in request to calculator: {logical_error_description}")
